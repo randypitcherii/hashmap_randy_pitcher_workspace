f4f64f1f7b6f6fc7614c9dfbafb4785ca0fe99cd
Randy Pitcher <randypitcherii@gmail.com>
2020-07-30 15:09:17 -0500

    added makefile, reorged, removed junk, and added simpler ga modeling

---
+++ dbt/models/google_analytics/staging/stg_ga_hourly_traffic.sql
@@ -0,0 +1,16 @@
+{{ config(tags=["hourly"]) }}
+
+SELECT 
+  CONVERT_TIMEZONE('America/Chicago', DATE_HOUR)::TIMESTAMP_NTZ AS TRAFFIC_TIME_CT,
+  PROFILE                                                       AS TRAFFIC_PROFILE,
+  HOSTNAME                                                      AS TRAFFIC_HOSTNAME,
+  CAMPAIGN                                                      AS TRAFFIC_CAMPAIGN,
+  PAGE_PATH                                                     AS TRAFFIC_PAGE_PATH,
+  SOURCE_MEDIUM                                                 AS TRAFFIC_SOURCE_MEDIUM,
+  USERS                                                         AS TRAFFIC_TOTAL_USERS_CNT,
+  NEW_USERS                                                     AS TRAFFIC_NEW_USERS_CNT,
+  PAGEVIEWS                                                     AS TRAFFIC_PAGEVIEWS_CNT,
+  AVG_SESSION_DURATION                                          AS TRAFFIC_SESSION_DURATION_AVG
+
+FROM 
+  {{ source('google_analytics', 'hourly_traffic') }}

04a5d557a5c81063bbbcb7074a409381fd27ff45
Randy Pitcher <randypitcherii@gmail.com>
2020-07-31 17:16:54 -0500

    fixed data quality for google analytics

--- dbt/tests/google_analytics/assert_no_dev_traffic__ga_hourly_traffic.sql
+++ dbt/tests/google_analytics/assert_no_dev_traffic__ga_hourly_traffic.sql
@@ -6,4 +6,4 @@ FROM
 
 WHERE 
   -- remove development traffic
-  TRAFFIC_HOSTNAME IN ('127.0.0.1', 'localhost') 
+  TRAFFIC_HOSTNAME IN ('127.0.0.1', 'localhost', '0.0.0.0', 'recommender.hashmapinc.com.s3-website-us-east-1.amazonaws.com') 

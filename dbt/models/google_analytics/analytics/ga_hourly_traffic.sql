e29c5ba7eb1bc4a0ca9ccb8ab965f10b520a20bb
Randy Pitcher <randypitcherii@gmail.com>
2020-10-29 15:33:31 -0500

    fixed ambiguous filtering criteria in ga_hourly_traffic

--- dbt/models/google_analytics/analytics/ga_hourly_traffic.sql
+++ dbt/models/google_analytics/analytics/ga_hourly_traffic.sql
@@ -1,13 +1,21 @@
+WITH TRAFFIC AS (
+  SELECT
+    TRAFFIC_TIME_CT,
+    REPLACE(TRAFFIC_HOSTNAME, 'www.', '') AS TRAFFIC_HOSTNAME,
+    TRAFFIC_CAMPAIGN,
+    REPLACE(TRAFFIC_PAGE_PATH, 'datarebels', 'hashmap') AS TRAFFIC_PAGE_PATH_RAW,
+    TRAFFIC_SOURCE_MEDIUM,
+    TRAFFIC_TOTAL_USERS_CNT,
+    TRAFFIC_NEW_USERS_CNT,
+    TRAFFIC_PAGEVIEWS_CNT,
+    TRAFFIC_SESSION_DURATION_AVG
+  
+  FROM
+    {{ ref('stg_ga_hourly_traffic') }}
+)
+
 SELECT
-  TRAFFIC_TIME_CT,
-  REPLACE(TRAFFIC_HOSTNAME, 'www.', '') AS TRAFFIC_HOSTNAME,
-  TRAFFIC_CAMPAIGN,
-  REPLACE(TRAFFIC_PAGE_PATH, 'datarebels', 'hashmap') AS TRAFFIC_PAGE_PATH_RAW,
-  TRAFFIC_SOURCE_MEDIUM,
-  TRAFFIC_TOTAL_USERS_CNT,
-  TRAFFIC_NEW_USERS_CNT,
-  TRAFFIC_PAGEVIEWS_CNT,
-  TRAFFIC_SESSION_DURATION_AVG,
+  *,
 
   -- clean traffic metadata
   SPLIT_PART(TRAFFIC_PAGE_PATH_RAW, '?', 1)     AS TRAFFIC_PAGE_BASE_PATH,
@@ -16,12 +24,12 @@ SELECT
 
   -- Prepend hostname to page path when hostname is not the main hashmap site
   CASE 
-      WHEN TRAFFIC_HOSTNAME = 'www.hashmapinc.com' THEN TRAFFIC_PAGE_BASE_PATH
-      ELSE TRAFFIC_HOSTNAME || TRAFFIC_PAGE_BASE_PATH
+    WHEN TRAFFIC_HOSTNAME = 'www.hashmapinc.com' THEN TRAFFIC_PAGE_BASE_PATH
+    ELSE TRAFFIC_HOSTNAME || TRAFFIC_PAGE_BASE_PATH
   END AS TRAFFIC_PAGE
 
 FROM
-  {{ ref('stg_ga_hourly_traffic') }}
+  TRAFFIC
 
 WHERE 
   -- only include traffic from production hostnames - this excludes `localhost`, `0.0.0.0`, etc...
